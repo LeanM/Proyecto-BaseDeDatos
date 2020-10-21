e6f36971cfcbf8e23f94533940d6fb1495197945
Leandro <leandro.moran.11@gmail.com>
2020-10-21 16:48:16 -0300

    Ventana admin termina3

--- src/GUI/Ventanas/VentanaAdmin.java
+++ src/GUI/Ventanas/VentanaAdmin.java
@@ -11,6 +11,8 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.sql.*;
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
@@ -28,8 +30,9 @@ public class VentanaAdmin extends JFrame {
 
     private JPanel contentPane;
     private JTextArea textoConsultas;
+    private JScrollPane scroll_texto_Consulta;
     private JButton realizarConsulta;
-    private JLabel mensajeError;
+    private JLabel etiquetaTablas;
     private JList listaTablas,listaAtributosTabla;
     private DefaultListModel<String> model_Lista_Tablas,model_Lista_Atrubutos_Tabla;
     private DBTable tabla;
@@ -48,15 +51,25 @@ public class VentanaAdmin extends JFrame {
         setResizable(false);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setBackground(Color.DARK_GRAY);
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
+        //Creo bordes
+        Border raisedbevel = BorderFactory.createRaisedBevelBorder();
+        Border loweredbevel = BorderFactory.createLoweredBevelBorder();
+        Border compuesto = BorderFactory.createCompoundBorder(raisedbevel,loweredbevel);
+
         textoConsultas = new JTextArea();
-        textoConsultas.setBounds(30, 5, 800, 150);
         textoConsultas.setBackground(Color.LIGHT_GRAY);
+        textoConsultas.setBorder(compuesto);
+
+        scroll_texto_Consulta = new JScrollPane(textoConsultas,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll_texto_Consulta.setBackground(Color.DARK_GRAY);
+        scroll_texto_Consulta.setBounds(30,5,1100,150);
 
         realizarConsulta = new JButton("Consultar");
-        realizarConsulta.setBounds(850,30,100,50);
+        realizarConsulta.setBounds(1150,30,150,50);
         realizarConsulta.setBackground(Color.DARK_GRAY);
         realizarConsulta.setForeground(Color.WHITE);
 
@@ -68,19 +81,23 @@ public class VentanaAdmin extends JFrame {
         });
 
 
-        mensajeError = new JLabel();
-        mensajeError.setBounds(1150, 500, 200, 100);
-        mensajeError.setForeground(Color.RED);
+        etiquetaTablas = new JLabel("Tablas de la base de datos :");
+        etiquetaTablas.setBounds(1100,200,200,20);
+        etiquetaTablas.setForeground(Color.WHITE);
 
         listaAtributosTabla = new JList();
-        listaAtributosTabla.setBounds(1000, 300, 100, 200);
+        listaAtributosTabla.setBounds(1120, 465, 150, 200);
         listaAtributosTabla.setBackground(Color.lightGray);
+        listaAtributosTabla.setBorder(compuesto);
+
         model_Lista_Atrubutos_Tabla = new DefaultListModel<String>();
         listaAtributosTabla.setModel(model_Lista_Atrubutos_Tabla);
 
         listaTablas = new JList();
-        listaTablas.setBounds(775, 300, 200, 300);
+        listaTablas.setBounds(1100, 225, 200, 225);
         listaTablas.setBackground(Color.lightGray);
+        listaTablas.setBorder(compuesto);
+
         model_Lista_Tablas = new DefaultListModel<String>();
         listaTablas.setModel(model_Lista_Tablas);
 
@@ -96,13 +113,14 @@ public class VentanaAdmin extends JFrame {
         });
 
 
-        tabla.setBounds(30,170,500,500);
+        tabla.setBounds(30,170,1050,550);
         tabla.setBackground(Color.LIGHT_GRAY);
+        tabla.setBorder(compuesto);
         tabla.setEditable(false);
 
-        contentPane.add(textoConsultas);
+        contentPane.add(etiquetaTablas);
+        contentPane.add(scroll_texto_Consulta);
         contentPane.add(realizarConsulta);
-        contentPane.add(mensajeError);
         contentPane.add(tabla);
         contentPane.add(listaTablas);
         contentPane.add(listaAtributosTabla);
@@ -136,6 +154,9 @@ public class VentanaAdmin extends JFrame {
 
         } catch (SQLException ex) {
             // en caso de error, se muestra la causa en la consola
+            JOptionPane.showMessageDialog(this,
+                    "Se produjo un error al intentar realizar la consulta.\n" + ex.getMessage(),
+                    "Error", JOptionPane.ERROR_MESSAGE);
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
