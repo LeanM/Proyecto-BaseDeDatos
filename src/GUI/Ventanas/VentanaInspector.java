18bffca4f6d7668f2c0f7b8e7bd245b0dd1ebb7c
Nacho <54416746+NachoOlivero@users.noreply.github.com>
2020-10-23 02:32:14 -0300

    boton cargar
    ahi termine de agregar casi todo los del boton cargar patentes, creo que anda bien, lo unico que faltaria es agregar a la tabla de resultados el numero de multa, pero no se bien como hacerlo porque el inspector no tiene select en la tabla de multas

--- src/GUI/Ventanas/VentanaInspector.java
+++ src/GUI/Ventanas/VentanaInspector.java
@@ -15,9 +15,13 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import quick.dbtable.DBTable;
 
@@ -47,11 +51,23 @@ public class VentanaInspector extends JFrame{
 	protected JComboBox <String> calles;
 	protected ActionListener oyenteCalles;
 	protected JComboBox <String> alturas;
+	protected ActionListener oyenteAlturas;
+	protected JComboBox <String> parquimetros;
 	 
 	protected DefaultListModel <String> estacionados;
 	protected JList<String> listaE;
 	
 	protected DBTable tablaBD;
+	
+	protected String legajo;
+	protected String calle;
+	protected String altura;
+	//protected String parquimetro;  Me parece que es mejor no tener esta variable global asi no hay que hacer un oyente para parquimetros, y ahi actualizar el parquimetro elegido
+ 	
+	Connection conexion;
+    Statement stmt;
+    String sql;
+    ResultSet rs;
 	
 	 
 	public VentanaInspector(DBTable tabla) {
@@ -128,17 +144,24 @@ public class VentanaInspector extends JFrame{
 	     eliminar.addActionListener(oyenteEliminar);
 	     fondoIngresado.add(eliminar);
 	     
-	     calles = new JComboBox<String>();  //hay que cargar las ubicaciones como strings desde la bd, si es que se puede
+	     calles = new JComboBox<String>();  //hay que cargar las ubicaciones como strings desde la bd
 	     calles.setSelectedIndex(-1);
-	     calles.setBounds(900,300,200,50);
+	     calles.setBounds(900,250,200,50);
 	     oyenteCalles = new OyenteCalles();
 	     calles.addActionListener(oyenteCalles);
 	     fondoIngresado.add(calles);
 	     
-	     alturas = new JComboBox<String>(); //hay que cargar los parquimetros de la ubicacion que se elige como strings desde la bd, si es que se puede
+	     alturas = new JComboBox<String>(); //hay que cargar los parquimetros de la ubicacion que se elige como strings desde la bd
 	     alturas.setSelectedIndex(-1);
-	     alturas.setBounds(900,500,200,50);
+	     alturas.setBounds(900,350,200,50);
+	     oyenteAlturas = new OyenteAlturas();
+	     alturas.addActionListener(oyenteAlturas);
 	     fondoIngresado.add(alturas);
+	     
+	     parquimetros = new JComboBox<String>(); //hay que cargar los parquimetros de la ubicacion que se elige como strings desde la bd
+	     parquimetros.setSelectedIndex(-1);
+	     parquimetros.setBounds(900,450,200,50);
+	     fondoIngresado.add(parquimetros);
 	     
 	     finalizarCargaPatentes = new JButton("Finalizar Carga");
 	     finalizarCargaPatentes.setBounds(900,650,200,50);
@@ -166,20 +189,19 @@ public class VentanaInspector extends JFrame{
 	protected void actualizarAlturas() { // Selecciona las alturas disopibles para la calle elegida
 		
 		try {
-			String calle = (String)calles.getSelectedItem(); 
-			
-		    Connection conexion = tablaBD.getConnection();
-		    Statement stmt = conexion.createStatement();
-            String sql = "SELECT altura FROM parquimetros WHERE calle='"+calle+"'";
-            ResultSet rs = stmt.executeQuery(sql);
+		    conexion = tablaBD.getConnection();
+		    stmt = conexion.createStatement();
+            sql = "SELECT altura FROM parquimetros WHERE calle='"+calle+"'";
+            rs = stmt.executeQuery(sql);
+            
             String [] alturasBD = new String [99999];
-           
             int index = 0;
+            DefaultComboBoxModel<String> model;
             
             while(rs.next()) 
             	alturasBD[index++]=rs.getString("altura");
             
-            DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>(eliminarEspacios(index,alturasBD));
+            model = new DefaultComboBoxModel<String>(eliminarEspacios(index,alturasBD));
             alturas.setModel(model);
            
             stmt.close();
@@ -187,15 +209,243 @@ public class VentanaInspector extends JFrame{
 		}
 		
 		catch (SQLException ex) {
+			System.out.println("Error al actualizar las alturas");
 			System.out.println("SQLException: " + ex.getMessage());
 			System.out.println("SQLState: " + ex.getSQLState());
 			System.out.println("VendorError: " + ex.getErrorCode());
 		}
 		
+		altura=(String)alturas.getSelectedItem();
 		checkFinalizable();
 	}
 	
+	protected void actualizarParquimetros() { // Selecciona los parquimetros disponibles para la calle y alturas elegidas
+		
+		try {
+		    conexion = tablaBD.getConnection();
+		    stmt = conexion.createStatement();
+            sql = "SELECT id_parq FROM parquimetros WHERE calle='"+calle+"' and altura='"+altura+"'";
+            rs = stmt.executeQuery(sql);
+            
+            String [] parquimetrosBD = new String [99999];
+            int index = 0;
+            DefaultComboBoxModel<String> model;
+            
+            while(rs.next()) 
+            	parquimetrosBD[index++]=rs.getString("id_parq");
+            
+            model = new DefaultComboBoxModel<String>(eliminarEspacios(index,parquimetrosBD));
+            parquimetros.setModel(model);
+           
+            stmt.close();
+            rs.close();
+		}
+		
+		catch (SQLException ex) {
+			System.out.println("Error al actualizar los parquimetros");
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		//parquimetro=(String)parquimetros.getSelectedItem();
+		checkFinalizable();
+	}
+	
+	protected String[] habilitado() {
+	
+		String ret[]=null;
+		String dia=null;
+		String turno=null;
+		String fecha=null;
+		String hora=null;
+		String parquimetro=(String) parquimetros.getSelectedItem();
+		
+		try {
+		    conexion = tablaBD.getConnection();
+		    stmt = conexion.createStatement();
+            sql = "SELECT curtime(),curdate(),hour(curtime()),dayofweek(curdate())";
+            rs = stmt.executeQuery(sql);
+            
+            if(rs.next()) {
+            	hora="09:05:04";//rs.getString(1);
+            	fecha="2020-10-20";//rs.getString(2);
+            	turno="m";//turno(rs.getInt(3));
+            	dia="ma";//dia(rs.getInt(4));
+            }
+            	
+            stmt.close();
+            rs.close();      
+		}
+		catch (SQLException ex) {
+			System.out.println("Error al buscar la fecha y hora");
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+        //-----------------------------------------------------------------------------------------------//
+		 try {
+			conexion = tablaBD.getConnection();System.out.println(legajo+calle+altura+parquimetro);
+		    stmt = conexion.createStatement();
+            sql = "SELECT * FROM asociado_con WHERE legajo='"+legajo+"'";
+            rs = stmt.executeQuery(sql);
+            
+            while(rs.next()) 
+            	if(rs.getString("calle").equals(calle) && rs.getString("altura").equals(altura) && 
+            	   rs.getString("dia").equals(dia) && rs.getString("turno").equals(turno)) {
+            		
+            		ret= new String[3];
+            		ret[0]=fecha;
+            		ret[1]=hora;
+            		ret[2]=rs.getString("id_asociado_con");
+            	}
+           
+            stmt.close();
+            rs.close();
+		}
+		
+		catch (SQLException ex) {
+			System.out.println("Error al buscar las ubicaciones del permitadas para el legajo");
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		return ret;
+	}
+	
+	protected String turno (int hora) {
+		
+		String ret;
+		
+		if(8<=hora && hora<=13)
+			ret="m";
+		else 
+			if(14<=hora && hora<=19) 
+				ret="t";
+			 else ret=null;
+		
+		return ret;
+		
+	}
+	
+	protected String dia (int numero) {
+		
+		String dia=null;
+		
+		switch (numero) {
+			case 1: dia="lu";
+			break;
+			case 2: dia="ma";
+			break;
+			case 3: dia="mi";
+			break;
+			case 4: dia="ju";
+			break;
+			case 5: dia="vi";
+			break;
+			case 6: dia="sa";
+			break;
+			case 7: dia="do";
+			break;
+		}
+		
+		return dia;
+	}
+	
+	protected void registrarAcceso(String fecha,String hora) {
+		
+		String parquimetro=(String) parquimetros.getSelectedItem();
+		
+		try {
+			conexion = tablaBD.getConnection();
+		    stmt = conexion.createStatement();
+            sql = "INSERT INTO accede(legajo, id_parq, fecha, hora) VALUES ("+legajo+","+parquimetro+",'"+fecha+"','"+hora+"');";
+            stmt.execute(sql);
+  
+            stmt.close();
+		}
+		
+		catch (SQLException ex) {
+			System.out.println("Error al insertar acceso a parquimetro");
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+	}
+	
+	protected DefaultTableModel generarMultas(String fecha,String hora,String id_asociado_con) {
+		
+		int cantEstacionados=0;
+		int cantAnotados=listaE.getModel().getSize();
+		String patentesEstacionadas[] = new String[99999];
+		String patenteActual;
+		boolean estaEnInfraccion=true;
+		
+		String[] nombresColumnas = {"Fecha","Hora","Calle","Altura","Patente","Legajo inspector"};
+		DefaultTableModel model = new DefaultTableModel(nombresColumnas, 0);
+
+		
+		//String [][] multasCreadas;
+		
+		try {
+			conexion = tablaBD.getConnection();
+		    stmt = conexion.createStatement();
+            sql = "SELECT patente FROM estacionados WHERE calle='"+calle+"' and altura='"+altura+"'";
+            rs = stmt.executeQuery(sql);
+           
+            while(rs.next()) {
+            	patentesEstacionadas[cantEstacionados++]=rs.getString("patente");
+            }
+            
+            stmt.close();
+            rs.close();
+		}
+		
+		catch (SQLException ex) {
+			System.out.println("Error al recuperar patentes estacionadas");
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		
+		
+		//multasCreadas = new String[cantAnotados][7];
+		
+		//int pointerMultas = 0;
+		
+		for(int i=0;i<cantAnotados;i++) {
+			int j=0;
+			patenteActual=listaE.getModel().getElementAt(i);
+			
+			while(estaEnInfraccion && j<cantEstacionados) 
+				estaEnInfraccion = !patenteActual.equals(patentesEstacionadas[j++]);
+			
+			if(estaEnInfraccion)
+				try {					
+					conexion = tablaBD.getConnection();
+				    stmt = conexion.createStatement();
+		            sql = "INSERT INTO multa(fecha, hora, patente, id_asociado_con) VALUES ('"+fecha+"','"+hora+"','"+patenteActual+"',"+id_asociado_con+")";
+		            stmt.execute(sql);
+		            
+		            model.addRow(new Object[] {fecha,hora,calle,altura,patenteActual,legajo}); //faltaría el numero de multa
+		            	           	            
+		            stmt.close();
+		         }
+				
+				catch (SQLException ex) {
+					System.out.println("Error al insertar multa");
+					System.out.println("SQLException: " + ex.getMessage());
+					System.out.println("SQLState: " + ex.getSQLState());
+					System.out.println("VendorError: " + ex.getErrorCode());
+				}
+			}	
+		
+		return model;
+	}
+	
 	protected String [] eliminarEspacios (int cant,String original []) {
+		
 		String nuevo [] = new String [cant];
 		
 		for(int i=0;i<cant;i++)
@@ -216,32 +466,34 @@ public class VentanaInspector extends JFrame{
 
 		public void actionPerformed(ActionEvent e) {
 			
-			String legajo, pwd;
+			String legajoIngresado = textoLegajo.getText();
+			String pwd = textoContraseña.getText();;
 			boolean legajoValido=false;
 			boolean pwdValida=false;
+			String [] callesBD;
+			DefaultComboBoxModel<String> model;
 			
-			legajo=textoLegajo.getText();
-			pwd=textoContraseña.getText();
-			
-			if( legajo.isEmpty()|| pwd.isEmpty() ) {
+			if( legajoIngresado.isEmpty()|| pwd.isEmpty() ) {
 				JOptionPane.showMessageDialog(null,"Por favor, complete ambos campos");
 				return;
 			}
 			
 			try {
-			    Connection conexion = tablaBD.getConnection();
-	            Statement stmt = conexion.createStatement();
-	            String sql = "SELECT legajo,password FROM inspectores";
-	            ResultSet rs = stmt.executeQuery(sql);
+			    conexion = tablaBD.getConnection();
+	            stmt = conexion.createStatement();
+	            sql = "SELECT legajo,password FROM inspectores";
+	            rs = stmt.executeQuery(sql);
 	            
 	            while(rs.next() && !legajoValido) 
-	            	if(rs.getString("legajo").equals(legajo)) {
+	            	if(rs.getString("legajo").equals(legajoIngresado)) {
 	            		legajoValido=true;
 	            		pwdValida=rs.getString("password").equals(pwd);
 	            	}
+	            
 	            stmt.close();
 	            rs.close();
 			}
+			
 			catch (SQLException ex) {
 				System.out.println("SQLException: " + ex.getMessage());
 				System.out.println("SQLState: " + ex.getSQLState());
@@ -262,18 +514,18 @@ public class VentanaInspector extends JFrame{
 			}
 			
 			try {
-			    Connection conexion = tablaBD.getConnection();
-			    Statement stmt = conexion.createStatement();
-	            String sql = "SELECT calle FROM parquimetros";
-	            ResultSet rs = stmt.executeQuery(sql);
-	            String [] callesBD = new String [99999];
-	           
+			    conexion = tablaBD.getConnection();
+			    stmt = conexion.createStatement();
+	            sql = "SELECT calle FROM parquimetros";
+	            rs = stmt.executeQuery(sql);
+	            
+	            callesBD = new String [99999];
 	            int index = 0;
 	            
 	            while(rs.next()) 
 	            	callesBD[index++]=rs.getString("calle");
 	            
-	            DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>(eliminarEspacios(index,callesBD));
+	            model = new DefaultComboBoxModel<String>(eliminarEspacios(index,callesBD));
 	            calles.setModel(model);
 	           
 	            stmt.close();
@@ -286,7 +538,10 @@ public class VentanaInspector extends JFrame{
 				System.out.println("VendorError: " + ex.getErrorCode());
 			}
 			
+			legajo=legajoIngresado;
+			calle=(String)calles.getSelectedItem();
 			actualizarAlturas();
+			actualizarParquimetros();
 			setContentPane(fondoIngresado);
 		    validate();
 			
@@ -337,7 +592,18 @@ public class VentanaInspector extends JFrame{
 
 		
 		public void actionPerformed(ActionEvent e) {
+			calle=(String)calles.getSelectedItem();
 			actualizarAlturas();
+			actualizarParquimetros();
+		}
+		
+	}
+	
+protected class OyenteAlturas implements ActionListener {
+
+		
+		public void actionPerformed(ActionEvent e) {
+			actualizarParquimetros();
 		}
 		
 	}
@@ -347,14 +613,20 @@ public class VentanaInspector extends JFrame{
 	
 		public void actionPerformed(ActionEvent arg0) { // Hay que checkear que el inspector pueda hacer multas en la hora y lugar que esta intentando, y si puede, simular la conexion
 			
-			/*if(habilitado()) {
-				registrarAcceso();
-				generarMultas();
-				mostrarMultas();
+			String datos[] = habilitado();  //datos[0] es la fecha, datos[1] es la hora. datos[2] es el id_asociado_con
+			TableModel modelo;  // aca se guardan las multas generadas, si es que hay
+			
+			if(datos!=null) {
+				registrarAcceso(datos[0],datos[1]);
+				modelo = generarMultas(datos[0],datos[1],datos[2]);
+				
+				if(modelo.getRowCount()>0)
+					JOptionPane.showMessageDialog(null, new JScrollPane(new JTable(modelo)));
+				else JOptionPane.showMessageDialog(null, "No hay autos en infraccion entre los cargados");
 			}
 			else {
 				JOptionPane.showMessageDialog(null,"No esta habilitado para labar multas en esta ubicacion");
-			}*/
+			}
 		}
 		
 	}
